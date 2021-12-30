4bc9af0f264dcaa6de4f0da28737a41eac878f6e
zakria <zakria.jan@10pearls.com>
2021-12-30 18:52:05 +0500

    removed unused vars

--- src/components/SubscriptionForm/usePostForm.tsx
+++ src/components/SubscriptionForm/usePostForm.tsx
@@ -1,10 +1,6 @@
 import { useMutation } from "react-query";
 import { Subscription } from "../../utils/types";
 import axios from "axios";
-type postFormProps = {
-  url: string;
-  subscription: Subscription;
-};
 const postForm = (url: string, subscription: Subscription): Promise<Response> =>
   axios.post(url, subscription);
 const usePostForm = () => {
