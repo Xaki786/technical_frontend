dfdf19a75a7a6f4ca9b3fd366db5387358bfb540
zakria <zakria.jan@10pearls.com>
2021-12-30 10:48:11 +0500

    testing backend deployed connection

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,16 @@ import Navbar from "./components/Navbar/Navbar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { routes } from "./routes/routes";
 import { Container } from "@mui/material";
+import axios from "axios";
+import { useEffect } from "react";
 function App() {
+  useEffect(() => {
+    const fetchApi = async () => {
+      const res = await axios.get("https://technical-backend.herokuapp.com/");
+      console.log({ res });
+    };
+    fetchApi();
+  }, []);
   return (
     <div className="App">
       <Router>
