5235ac1fb91a95e105a6a7b6f5cc13e78015ee15
zakria <zakria.jan@10pearls.com>
2022-01-02 10:09:33 +0500

    converted backend url to production

--- src/utils/Axios.ts
+++ src/utils/Axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 const Axios = axios.create({
-  baseURL: "http://localhost:5000",
-  // baseURL: "https://technical-backend.herokuapp.com",
+  // baseURL: "http://localhost:5000",
+  baseURL: "https://technical-backend.herokuapp.com",
 });
 export default Axios;
